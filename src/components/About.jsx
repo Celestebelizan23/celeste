de2363a670e45be4fb080022627101446565d78a
Celestebelizan23 <celestebelizan23@gmail.com>
2023-02-13 11:12:32 -0300

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-purple-900 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4  border-purple-400">
+         About
+          </p>
+        </div>
+
+        <p className="text-xl mt-20">
+
+      As a web developer, I have a deep passion for creating impactful and meaningful user experiences
+      that provide innovative and efficient solutions to my clients.
+      Working in a team with people from different disciplines is one of my strengths and I am proud to 
+      contribute to the success of each project.
+      Before starting any project, I make an effort to fully understand my clients' needs and goals. 
+      I want to ensure that each solution I offer is personalized and effective, and to do that, 
+      it is essential to understand their challenges and expectations.
+      I like to work tirelessly to provide solutions that meet my clients'
+      expectations and have a positive impact on their lives.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Home.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import Cele from "../assets/cele.png";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { Link } from "react-scroll";
+
+const Home = () => {
+  return (
+    <div
+      name="home"
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-purple-900"
+    >
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className="flex flex-col justify-center h-full">
+          <h2 className="text-4xl sm:text-7xl font-bold text-white">
+            I'm a Front End Developer
+          </h2>
+          <p className="text-purple-200 py-4 max-w-md">
+          Busco un puesto desafiante que me permita crecer en lo que 
+          me apasiona que es el desarrollo web y seguir capacitándome 
+          cada vez en el campo dinámico de la tecnología de la información.
+          </p>
+
+          <div>
+            <Link
+              to="portfolio"
+              smooth
+              duration={500}
+              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-black cursor-pointer"
+            >
+              Portfolio
+              <span className="group-hover:rotate-90 duration-300">
+                <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
+              </span>
+            </Link>
+          </div>
+        </div>
+
+        <div>
+          <img
+            src={Cele}
+            alt="my profile"
+            className="rounded-2xl mx-auto w-2/3 md:w-full"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
