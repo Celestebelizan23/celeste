d57dd70612434d3605962935239ece6ec0c2b905
Celestebelizan23 <celestebelizan23@gmail.com>
2023-02-16 23:02:01 -0300

    restructuring code

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -15,9 +15,9 @@ const Portfolio = () => {
   return (
     <div
       name="portfolio"
-      className="bg-gradient-to-b from-black to-purple-900 w-full text-white md:h-screen"
+      className="bg-gradient-to-b from-black to-purple-900 w-full text-white md:h-screen flex flex-col"
     >
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+      <div className="max-w-screen-lg p-4 mx-auto">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-purple-400">
             Portfolio
@@ -25,7 +25,7 @@ const Portfolio = () => {
           
         </div>
 
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+        <div className="grid sm:grid-cols-1 md:grid-cols-3 gap-8 px-12 sm:px-0">
           {portfolios.map(({ id, src, link, code }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               <img
